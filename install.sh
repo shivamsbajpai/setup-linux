9f03cf588f3789b8dbdd9ebbb0779a1c0901cfcb
Shivam <f20160711@hyderabad.bits-pilani.ac.in>
2023-10-30 21:36:25 +0530

    added git

--- install.sh
+++ install.sh
@@ -1,3 +1,38 @@
 #!/bin/bash
-sudo mkdir -p /etc/firefox/policies
-sudo cp policies.json /etc/firefox/policies/policies.json
+echo "=============================================="
+echo "updating package manager index..."
+echo "=============================================="
+# sudo apt update
+## firefox
+echo "=============================================="
+echo "updating firefox policies..."
+echo "=============================================="
+# sudo mkdir -p /etc/firefox/policies
+# sudo cp firefox_policies.json /etc/firefox/policies/policies.json
+# ## vs code
+echo "=============================================="
+echo "installing vs code..."
+echo "=============================================="
+# sudo apt-get install wget gpg
+# wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+# sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+# sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+# rm -f packages.microsoft.gpg
+# sudo apt install apt-transport-https
+# sudo apt update
+# sudo apt install code
+# ## vs code settings
+# cp vs_code_settings.json $HOME/.config/Code/User/settings.json
+# ## install git
+# echo "=============================================="
+# echo "installing git..."
+# echo "=============================================="
+# sudo apt install git -y
+# echo "=============================================="
+# echo "setup complete"
+# echo "=============================================="
+source env.sh
+echo "configuring git"
+echo "=============================================="
+git config user.email "$GIT_EMAIL"
+git config user.name "$GIT_NAME"

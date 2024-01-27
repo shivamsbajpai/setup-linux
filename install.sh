16d134cdb64d76c56a435dbf83d3c5775100a802
Shivam <ssbajpai9+github@gmail.com>
2024-01-27 11:09:27 +0530

    added blueman

--- install.sh
+++ install.sh
@@ -6,16 +6,70 @@ echo "=============================================="
 sudo apt update
 ### following command will update the packages and dependencies
 sudo apt upgrade
+###curl
+echo "=============================================="
+echo "installing curl"
+echo "=============================================="
+sudo snap install curl
+###vim
 echo "=============================================="
 echo "installing vim"
 echo "=============================================="
 sudo apt-get install vim
+###vlc
+echo "=============================================="
+echo "installing vlc"
+echo "=============================================="
+sudo snap install vlc
+###install git
+echo "=============================================="
+echo "installing git..."
+echo "=============================================="
+sudo apt install git -y
 ###firefox
 echo "=============================================="
 echo "updating firefox policies..."
 echo "=============================================="
 sudo mkdir -p /etc/firefox/policies
 sudo cp firefox_policies.json /etc/firefox/policies/policies.json
+###dropbox
+echo "=============================================="
+echo "downloading dropbox"
+echo "=============================================="
+wget -O dropbox.deb https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2020.03.04_amd64.deb
+echo "=============================================="
+echo "installing dropbox"
+echo "=============================================="
+sudo dpkg -i dropbox.deb
+###uninstall dropbox
+####sudo apt-get remove dropbox
+###install authy
+echo "=============================================="
+echo "installing authy"
+echo "=============================================="
+sudo snap install authy
+###golang
+echo "=============================================="
+echo "installing go"
+echo "=============================================="
+sudo snap install go --classic
+###install google chrome
+echo "=============================================="
+echo "installing google chrome"
+echo "=============================================="
+###to avoid adding google repository
+sudo touch /etc/default/google-chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+echo "=============================================="
+echo "removing google chrome downloaded file"
+echo "=============================================="
+sudo rm google-chrome-stable_current_amd64.deb
+###blueman
+echo "=============================================="
+echo "install bluetooth manager"
+echo "=============================================="
+sudo apt-get install blueman
 ###vs code
 echo "=============================================="
 echo "installing vs code..."
@@ -33,11 +87,6 @@ sudo cp vs_code_settings.json $HOME/.config/Code/User/settings.json
 ###install vs code extensions
 code --install-extension firefox-devtools.vscode-firefox-debug
 mkdir ~/.firefox-debug-profile
-###install git
-echo "=============================================="
-echo "installing git..."
-echo "=============================================="
-sudo apt install git -y
 echo "=============================================="
 echo "setup complete"
 echo "=============================================="
@@ -50,21 +99,6 @@ echo "configuring git"
 echo "=============================================="
 git config --global user.email "$GIT_EMAIL"
 git config --global user.name "$GIT_NAME"
-echo "=============================================="
-echo "downloading dropbox"
-echo "=============================================="
-wget -O dropbox.deb https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2020.03.04_amd64.deb
-echo "=============================================="
-echo "installing dropbox"
-echo "=============================================="
-sudo dpkg -i dropbox.deb
-###uninstall dropbox
-####sudo apt-get remove dropbox
-###install authy
-echo "=============================================="
-echo "installing authy"
-echo "=============================================="
-sudo snap install authy
 echo "=============================================="
 echo "customizing ui"
 echo "=============================================="
@@ -100,27 +134,3 @@ echo "=============================================="
 npm install --global web-ext
 ###uninstallation command
 ####npm uninstall --global web-ext
-###install google chrome
-echo "=============================================="
-echo "installing google chrome"
-echo "=============================================="
-###to avoid adding google repository
-sudo touch /etc/default/google-chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-echo "=============================================="
-echo "removing google chrome downloaded file"
-echo "=============================================="
-sudo rm google-chrome-stable_current_amd64.deb
-echo "=============================================="
-echo "installing go"
-echo "=============================================="
-sudo snap install go --classic
-echo "=============================================="
-echo "installing vlc"
-echo "=============================================="
-sudo snap install vlc
-echo "=============================================="
-echo "installing curl"
-echo "=============================================="
-sudo snap install curl
